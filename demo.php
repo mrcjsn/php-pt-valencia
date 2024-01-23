a295880d723c53358257f143fc2ca488cd6343d7
unknown <jesonmarc3@gmail.com>
2024-01-22 18:30:30 -0800

    New File

---
+++ demo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Simple Form Processing</title>
+    </head>
+
+    <body>
+        <form action="form_1.php" method="post">
+            FirstName:
+            <input type="text" name="firstname" required/>
+            <br>
+            <br>
+            LastName:
+            <input type="text" name="lastname" required/>
+            <br>
+            <br>
+            Address:
+            <input type="text" name="address" required/>
+            <br>
+            <br>
+            Email Address:
+            <input type="text" name="email" required/>
+            <br>
+            <br>
+            Password:
+            <input type="text" name="password" required/>
+            <br>
+            <br>
+            <input type="submit" value=Submit>
+    </form>
+</body>
+</html>
